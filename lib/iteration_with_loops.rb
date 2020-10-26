79f1c488b1d9f9355e972c20b93a286d911c9727
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-26 22:12:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,9 @@ def find_min_in_nested_arrays(src)
   
   row_index = 0
   array = []
-  
   while row_index < src.count do
+    element_index = 0 
+    array[row_index] = [row_index][element_index]
     while element_index < src[row_index].count do
       if min_value > src[row_index][element_index]
         min_value = src[row_index][element_index]
